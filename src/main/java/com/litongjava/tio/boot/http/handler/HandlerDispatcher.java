d0572fe51f8a0a3b4a9b48ba84fa917cec002112
litongjava <litongjava@qq.com>
2023-12-22 01:10:05 -1000

    change to reflectasm

--- src/main/java/com/litongjava/tio/boot/http/handler/HandlerDispatcher.java
+++ src/main/java/com/litongjava/tio/boot/http/handler/HandlerDispatcher.java
@@ -1,7 +1,6 @@
 package com.litongjava.tio.boot.http.handler;
 
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -70,21 +69,8 @@ public class HandlerDispatcher {
     Object obj = null;
     if (parameterTypes == null || parameterTypes.length == 0) { // 无请求参数
       // 使用Java原生的方法进行调用
-      try {
-        log.info("controllerBean{},", controllerBean);
-        obj = actionMethod.invoke(controllerBean, null);
-      } catch (IllegalAccessException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (IllegalArgumentException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-//      obj = TioBootHttpRoutes.BEAN_METHODACCESS_MAP.get(controllerBean).invoke(controllerBean, actionMethod.getName(),
-//          parameterTypes, (Object) null);
+      obj = TioBootHttpRoutes.BEAN_METHODACCESS_MAP.get(controllerBean).invoke(controllerBean, actionMethod.getName(),
+          parameterTypes, (Object) null);
 
     } else {
       // 赋值这段代码待重构，先用上
