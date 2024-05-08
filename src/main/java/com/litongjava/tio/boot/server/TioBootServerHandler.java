6d130305c32473316312b48bbc82ababc30fd643
litongjava <litongjava@qq.com>
2024-05-08 09:15:25 -1000

    add sse

--- src/main/java/com/litongjava/tio/boot/server/TioBootServerHandler.java
+++ src/main/java/com/litongjava/tio/boot/server/TioBootServerHandler.java
@@ -11,10 +11,9 @@ import com.litongjava.tio.http.common.HttpConfig;
 import com.litongjava.tio.http.common.HttpRequest;
 import com.litongjava.tio.http.common.HttpRequestDecoder;
 import com.litongjava.tio.http.common.HttpResponse;
+import com.litongjava.tio.http.common.HttpResponsePacket;
 import com.litongjava.tio.http.common.handler.HttpRequestHandler;
 import com.litongjava.tio.http.server.HttpServerAioHandler;
-import com.litongjava.tio.http.server.sse.SseBytesPacket;
-import com.litongjava.tio.http.server.sse.SsePacket;
 import com.litongjava.tio.server.intf.ServerAioHandler;
 import com.litongjava.tio.websocket.common.WsRequest;
 import com.litongjava.tio.websocket.common.WsResponse;
@@ -126,15 +125,11 @@ public class TioBootServerHandler implements ServerAioHandler {
   public ByteBuffer encode(Packet packet, TioConfig tioConfig, ChannelContext channelContext) {
     if (packet instanceof HttpResponse) {
       return httpServerAioHandler.encode(packet, tioConfig, channelContext);
-      
-    } else if (packet instanceof SsePacket) {
-      SsePacket ssePacket = (SsePacket) packet;
-      return ssePacket.toByteBuffer(tioConfig);
-      
-    } else if (packet instanceof SseBytesPacket) {
-      SseBytesPacket ssePacket = (SseBytesPacket) packet;
-      return ssePacket.toByteBuffer(tioConfig);
-      
+
+    } else if (packet instanceof HttpResponsePacket) {
+      HttpResponsePacket responsePacket = (HttpResponsePacket) packet;
+      return responsePacket.toByteBuffer(tioConfig);
+
     } else if (packet instanceof WsResponse) {
       return wsServerAioHandler.encode(packet, tioConfig, channelContext);
     } else {
