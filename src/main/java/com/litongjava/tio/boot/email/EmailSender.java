ba9ed525ba2aa76425ec8bb7805a91e10862fdbb
litongjava <litongjava@qq.com>
2025-03-08 23:34:15 -1000

    update email sender

--- src/main/java/com/litongjava/tio/boot/email/EmailSender.java
+++ src/main/java/com/litongjava/tio/boot/email/EmailSender.java
@@ -5,4 +5,6 @@ public interface EmailSender {
   public boolean send(String to, String subject, String content);
 
   public boolean sendVerificationEmail(String to, String origin, String code);
+
+  public boolean sendVerificationCodeEmail(String email, String origin, String code);
 }
