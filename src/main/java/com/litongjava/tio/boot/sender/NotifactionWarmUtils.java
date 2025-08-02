c20ac8cde519e03f4553f6d811dbac60739636de
Tong Li <litongjava001@gmail.com>
2025-08-01 19:03:26 -1000

    update NotifactionWarmUtils

--- src/main/java/com/litongjava/tio/boot/sender/NotifactionWarmUtils.java
+++ src/main/java/com/litongjava/tio/boot/sender/NotifactionWarmUtils.java
@@ -12,15 +12,8 @@ import com.litongjava.tio.utils.network.IpUtils;
 import com.litongjava.tio.utils.notification.NotifactionWarmModel;
 
 public class NotifactionWarmUtils {
-  public static NotifactionWarmModel toWarmModel(String appGroupName, String warningName, String level, HttpRequest request, Throwable e) {
-    String requestId = request.getChannelContext().getId();
-
-    String requestLine = request.getRequestLine().toString();
-    String host = request.getHost();
-    String userId = request.getUserIdString();
-    String bodyString = request.getBodyString();
-
-    String realIp = HttpIpUtils.getRealIp(request);
+  public static NotifactionWarmModel toWarmModel(String appGroupName, String warningName, String level,
+      HttpRequest request, Throwable e) {
 
     // 获取完整的堆栈跟踪
     StringWriter sw = new StringWriter();
@@ -30,28 +23,33 @@ public class NotifactionWarmUtils {
 
     NotifactionWarmModel model = new NotifactionWarmModel();
 
+    if (request != null) {
+      String requestId = request.getChannelContext().getId();
+      model.setRequestId(requestId);
+      String requestLine = request.getRequestLine().toString();
+      String host = request.getHost();
+      String userId = request.getUserIdString();
+      String bodyString = request.getBodyString();
+
+      String realIp = HttpIpUtils.getRealIp(request);
+      model.setUserIp(realIp);
+      model.setUserId(userId);
+      model.setHost(host);
+      model.setReferer(request.getReferer());
+      model.setUserAgent(request.getUserAgent());
+      model.setRequestLine(requestLine);
+      model.setRequestBody(bodyString);
+    }
+
     String localIp = IpUtils.getLocalIp();
     String appName = EnvUtils.get(ServerConfigKeys.APP_NAME);
     model.setAppEnv(EnvUtils.env());
-
     model.setAppGroupName(appGroupName);
-
     model.setAppName(appName);
-
     model.setWarningName(warningName);
-
     model.setLevel(level);
-
     model.setDeviceName(localIp);
     model.setTime(ZonedDateTime.now());
-    model.setRequestId(requestId);
-    model.setUserIp(realIp);
-    model.setUserId(userId);
-    model.setHost(host);
-    model.setReferer(request.getReferer());
-    model.setUserAgent(request.getUserAgent());
-    model.setRequestLine(requestLine);
-    model.setRequestBody(bodyString);
     model.setStackTrace(stackTrace);
     return model;
   }
